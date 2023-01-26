73ef85ca3b4ca83aa1e4defe1bc2937aa6850991
amine <atlagh.amine@gmail.com>
2023-01-26 17:17:03 +0100

    click outside

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import {ReactComponent as ShopIcon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
@@ -6,13 +6,25 @@ import './cart-icon.styles.scss';
 const CartIcon = () => {
     const {isCartOpen, setIsCartOpen} = useContext(CartContext);
 
+    const refOne = useRef(null);
+
+    useEffect(()=> {
+        document.addEventListener("click", handleClickOutside, true)
+    });
+    
+    function handleClickOutside(e){
+        if(!refOne.current.contains(e.target)){
+            setIsCartOpen(false);
+        }
+    }
+    
     function toggleIsCartOpen(){
         setIsCartOpen(!isCartOpen);
 
     }
 
     return(
-        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+        <div className='cart-icon-container' ref={refOne} onClick={toggleIsCartOpen}>
             <ShopIcon className='shopping-icon'/>
             <span className='item-count'>0</span>
         </div>
