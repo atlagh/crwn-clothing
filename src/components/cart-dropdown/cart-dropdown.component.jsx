31a3c2b0204b4838ec23920635a99d62875f4492
amine <atlagh.amine@gmail.com>
2023-01-30 20:11:39 +0100

    testing vim

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,8 +5,8 @@ import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
 import { useNavigate } from 'react-router-dom';
 
-
-const CartDropdown = () => {
+ 
+const CartDropdown = () => { 
     const {cartItems, isCartOpen} = useContext(CartContext);
     const navigate = useNavigate();
 
