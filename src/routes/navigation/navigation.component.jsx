115925f98658983e0362eb8ccd270e905982432e
atlagh <59605306+atlagh@users.noreply.github.com>
2023-02-17 14:47:09 +0100

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
             </Link>
             <div className='nav-links-container'>
                 <Link className='nav-link' to='/shop'>
-                    S
+                    Shop
                 </Link>
                 {
                   currentUser ? (
